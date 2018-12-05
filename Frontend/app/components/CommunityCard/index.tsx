fb1ec980d1e35b9f803c18549f50d549335da975
Veronica <veronica.21.coutts@gmail.com>
2018-12-05 13:45:56 +0200

    Created card

---
+++ Frontend/app/components/CommunityCard/index.tsx
@@ -0,0 +1,27 @@
+/**
+ *
+ * CommunityCard
+ *
+ */
+
+import * as React from 'react';
+
+// import styled from 'styles/styled-components';
+
+
+interface OwnProps {
+  name: string;
+  tokens: number;
+  logo: string;
+  id: string;
+}
+
+const CommunityCard: React.SFC<OwnProps> = (props: OwnProps) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default CommunityCard;
