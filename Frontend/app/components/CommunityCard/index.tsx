028fd7b9d2ff9ff33f9e1dabfc34ba0a2b5fdf15
Veronica <veronica.21.coutts@gmail.com>
2018-12-05 14:06:42 +0200

    Added card layout

--- Frontend/app/components/CommunityCard/index.tsx
+++ Frontend/app/components/CommunityCard/index.tsx
@@ -5,23 +5,56 @@
  */
 
 import * as React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
-// import styled from 'styles/styled-components';
-
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
 
 interface OwnProps {
+  classes: any;
   name: string;
   tokens: number;
   logo: string;
   id: string;
 }
 
-const CommunityCard: React.SFC<OwnProps> = (props: OwnProps) => {
+function MediaCard(props: OwnProps) {
+  const { classes } = props;
   return (
-    <div>
-      
-    </div>
-  );
-};
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image= {props.logo}
+          title= {props.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.name}
+          </Typography>
+          <Typography component="p">
+            {props.tokens}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
 
-export default CommunityCard;
+      </CardActions>
+    </Card>
+  );
+}
+
+export default withStyles(styles,  { withTheme: true })(MediaCard);
+
